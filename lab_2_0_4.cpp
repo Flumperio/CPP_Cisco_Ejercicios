88d975ba1beb91cede7db3a1cf40277c75d84d08
Juan Carlos <jcs0969@icloud.com>
2025-06-25 18:58:23 +0200

    JC

--- lab_2_0_4.cpp
+++ lab_2_0_4.cpp
@@ -12,20 +12,72 @@ int main(void) {
     cnt = 0;
     cout << m << endl << ft << endl << in << endl << sys << endl ;
 
+    /* *************************************
+     ***** Entrada de datos ****************
+     ************************************ */
+
     do {
-    cout << "What system do you want use. 0 for metric, 1 for imperial: " << endl ;
-    cin >>  sys;
-    if ((sys != 0 and sys != 1) or cin.fail()) {
-        cout << "This is not a valid option" << endl ;
-        cin.clear();
-        cin.ignore(1000, '\n');
-        cnt += 1;
-    } else {
-        cnt = 10;
-    }
-    
+        cout << "What system do you want use. 0 for metric, 1 for imperial: " << endl ;
+        cin >>  sys;
+        if ((sys != 0 and sys != 1) or cin.fail()) {
+            cout << "This is not a valid option" << endl ;
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cnt += 1;
+        } else {
+            cnt = 10;
+        }
+
     } while (cnt < 3);
 
-    cout << sys;
-}
+    cout << sys << endl;
+    cnt = 0;
+    do {
 
+        if (sys == 0){
+            cout << "Please, input the meters, with decimal if you want, to converter: ";
+            cin >> m;
+            cnt += 1;
+        } else {
+            cout <<"Please, in0put the feet:";
+            cin >> ft;
+            cout << endl << "Please, input the inches, with decimals if you want: ";
+            cin >> in;
+            cnt += 1;
+        }
+        if (cin.fail()) {
+            cout << "You numbers is not correct. Please try again" << endl;
+            cin.clear();
+            cin.ignore(1000, '\n');
+        } else {
+            cnt = 10;
+        }
+
+    } while (cnt < 3);
+
+    /* *************************************
+     ***** Calculo de metrico a imperial ***
+     ************************************ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
