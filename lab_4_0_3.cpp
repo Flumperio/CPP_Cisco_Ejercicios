96dddbeebea04bc6fb50531700ac2b1583bf875b
Juan Carlos <jcs0969@icloud.com>
2025-08-20 12:28:04 +0200

    lab_4_0_3

--- lab_4_0_3.cpp
+++ lab_4_0_3.cpp
@@ -19,7 +19,7 @@ int entrada_datos (string Mensaje_Texto) {
 	int num_intro;
 	cout << Mensaje_Texto;
 	cin >> num_intro;
-	if ((num_intro < 1 or num_intro > 5) or cin.fail()){
+	if (cin.fail() || num_intro < 1){
 		cin.clear();
 		cin.ignore(1000, '\n');
 		cout << "Error. No está en el rango de numeros permitidos. Debe ser un número positivo entero";
@@ -27,16 +27,16 @@ int entrada_datos (string Mensaje_Texto) {
 	}
 	return num_intro;
 }
-void entrada_notas (avgr *Notas_Medias) {
+void entrada_notas (avgr &Notas_Medias) {
 	int cont_fila = 0, cont_col = 1;
-	Notas_Medias->n_asignaturas = entrada_datos(("Cuantas asignaturas has cursado: "));
-	Notas_Medias->puntuacion = new int *[Notas_Medias->n_asignaturas];
-	while (cont_fila < Notas_Medias->n_asignaturas) {
-		Notas_Medias->n_notas = entrada_datos("Cuantas notas tienes para la asignatura " + to_string(cont_fila + 1) + " : ");
-		Notas_Medias->puntuacion[cont_fila] = new int [Notas_Medias->n_notas + 1];
-		Notas_Medias->puntuacion[cont_fila][0] = Notas_Medias->n_notas;
-		while (cont_col <= Notas_Medias->n_notas) {
-			Notas_Medias->puntuacion[cont_fila][cont_col] = entrada_datos(to_string(cont_col+ 1) + ".- Nota para la asginatura " + to_string(cont_fila + 1) + ": ");
+	Notas_Medias.n_asignaturas = entrada_datos(("Cuantas asignaturas has cursado: "));
+	Notas_Medias.puntuacion = new int *[Notas_Medias.n_asignaturas];
+	while (cont_fila < Notas_Medias.n_asignaturas) {
+		Notas_Medias.n_notas = entrada_datos("Cuantas notas tienes para la asignatura " + to_string(cont_fila + 1) + " : ");
+		Notas_Medias.puntuacion[cont_fila] = new int [Notas_Medias.n_notas + 1];
+		Notas_Medias.puntuacion[cont_fila][0] = Notas_Medias.n_notas;
+		while (cont_col <= Notas_Medias.n_notas) {
+			Notas_Medias.puntuacion[cont_fila][cont_col] = entrada_datos(to_string(cont_col+ 1) + ".- Nota para la asginatura " + to_string(cont_fila + 1) + ": ");
 			cont_col++;
 		}
 		cont_fila ++;
@@ -46,12 +46,18 @@ void entrada_notas (avgr *Notas_Medias) {
 
 int main (){
 	avgr Notas_Medias = {0, 0, nullptr};
-	entrada_notas(&Notas_Medias);
+	float media_asignatura = 0.0, media_total = 0.0;
+	entrada_notas(Notas_Medias);
 	for (int f = 0; f < Notas_Medias.n_asignaturas; f++) {
 		for (int r = 1; r <= Notas_Medias.puntuacion[f][0]; r++) {
 			cout << Notas_Medias.puntuacion[f][r] << " ";
+			media_asignatura += Notas_Medias.puntuacion[f][r];
 		}
+		cout << "La media para la asignatura: " << f + 1 << " -- " << media_asignatura/Notas_Medias.puntuacion[f][0];
+		media_total += media_asignatura/Notas_Medias.puntuacion[f][0];
+		media_asignatura = 0;
 		cout << endl;
 	}
+	cout << "La media total es de : " << media_total / Notas_Medias.n_asignaturas << endl;
 	return 0;
 }
