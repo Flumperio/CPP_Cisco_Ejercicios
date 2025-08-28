1f0d0e81490584ff8045bebed55548ba0842f346
JCSantos <jcs0969@icloud.com>
2025-08-28 17:51:54 +0200

    lab_4_4_8.cpp

--- lab_4_4_8.cpp
+++ lab_4_4_8.cpp
@@ -3,30 +3,56 @@
 // Curso Cisco: c++ Essentials 1
 //  4.4.8   LAB   String in functions: IP numbers
 // *****************************************
+#include <cctype>
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 int n_octetos (const string &direccion_IP) {
-	size_t position = 0;
-	int n_octetos = 0;
-	position = direccion_IP.find('.');
+	int count = 0;
+	size_t position = direccion_IP.find('.');
 	while (position != string::npos) {
-		n_octetos++;
-		position += direccion_IP.find(position + 1, '.');
+		count++;
+		position = direccion_IP.find('.', position + 1);
 	}
-	return n_octetos;
+	return count;
 }
 
-int main () {
+bool check_octetos (const string &direccion_IP) {
+    for (char c : direccion_IP){
+        if (!isdigit(c) && c != '.')
+            return false;
+    }
+	return true;
+}
 
+bool check_numbers (const string &direccion_IP){
+    string octeto = direccion_IP.substr(0 , direccion_IP.find('.'));
+	string ip_rest = direccion_IP.substr(direccion_IP.find('.')+1);
+	size_t position = 0;
+    for (int contador = 0; contador < 4; contador ++) {
+	    if (atoi(octeto.c_str()) > 255)
+	    	return false;
+    	octeto = ip_rest.substr(0 , ip_rest.find('.'));
+    	ip_rest = ip_rest.substr(ip_rest.find('.')+1);
+    }
+    return true;
+}
+int main () {
 	string ip_entrada;
 	string octetos[3];
 	int numero_octetos = 0;
-	cout << "Dame la dirección IP completa para comprobarla:  ";
-	getline (cin, ip_entrada);
-	numero_octetos = n_octetos(ip_entrada);
-	cout << numero_octetos << endl;
+		cout << "Dame la dirección IP completa para comprobarla:  ";
+		getline (cin, ip_entrada);
+		if ((numero_octetos = n_octetos(ip_entrada)) !=3)
+			cout << "Numero de octetos erróneo - el formato es xxx.xxx.xxx.xxx" << endl;
+		else if (!check_octetos (ip_entrada))
+			cout << "Solo se permiten números separados por puntos" << endl;
+		else if (!check_numbers (ip_entrada))
+			cout << "Los números permitidos están entre 0 y 255." << endl;
+		else
+			cout << "La dirección IP: " << ip_entrada << " es correcta." << endl;
 	return 0;
 }
